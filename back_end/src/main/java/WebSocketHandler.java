629c7388ed9249264f0b477f9bf5a04590b7c853
Rhoanna <70310464+rhoannajo@users.noreply.github.com>
2020-12-04 15:20:45 -0800

    handles connecting to server

--- back_end/src/main/java/WebSocketHandler.java
+++ back_end/src/main/java/WebSocketHandler.java
@@ -11,19 +11,47 @@ import java.util.concurrent.ConcurrentHashMap;
 @WebSocket
 public class WebSocketHandler {
 
+    private static List<String> messages = new Vector<>();
+    private static Map<Session, Session> sessionMap = new ConcurrentHashMap<>();
+
+    public static void broadcast(String message) {
+        sessionMap.keySet().forEach(session -> {
+            try {
+                session.getRemote().sendString(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
     @OnWebSocketConnect
     public void connected(Session session) throws IOException {
+        sessionMap.put(session, session);
+        System.out.println("Client connected");
+        Gson gson = new Gson();
+        // tests we can see messages
+        session.getRemote().sendString(gson.toJson(messages));
     }
 
     @OnWebSocketClose
     public void closed(Session session, int statusCode, String reason){
+        // clears session
+        sessionMap.remove(session);
+        System.out.println("Client disconnected!");
     }
 
     @OnWebSocketMessage
     public void message(Session session, String message){
+        System.out.println("Client has sent: " + message);
+        // stores messages
+        messages.add(message);
+        //triggers broadcast
+        Gson gson = new Gson();
+        broadcast(gson.toJson(messages));
     }
 
     @OnWebSocketError
     public void error(Session session, Throwable error){
+        // shows error
+        System.out.println("Error communicating with server: " + error.getMessage());
     }
 }
