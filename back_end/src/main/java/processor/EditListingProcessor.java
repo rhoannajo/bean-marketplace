895f06cffa61e81bb3e9e0270e41672784f8321b
Rhoanna <70310464+rhoannajo@users.noreply.github.com>
2020-12-08 14:26:17 -0800

    added editListingProcessor to processors

---
+++ back_end/src/main/java/processor/EditListingProcessor.java
@@ -0,0 +1,27 @@
+package processor;
+
+import com.google.gson.Gson;
+import dao.ListingDao;
+import dto.ListingDto;
+import dto.ResponseDto;
+import parser.ParsedUrl;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class EditListingProcessor implements Processor {
+    private static Gson gson = new Gson();
+
+    @Override
+    public ResponseDto process(ParsedUrl parsedUrl, String body) {
+        // get id, delete and replace/add into same list
+        ListingDao lDao = ListingDao.getInstance();
+        lDao.delete(parsedUrl.getValue("id"));
+        //return new ResponseDto(new Date(), new ArrayList(), true);
+        ListingDto lDto = gson.fromJson(body, ListingDto.class);
+        List<ListingDto> list = new ArrayList<>();
+        list.add(lDao.put(lDto));
+        return new ResponseDto(new Date(), list, true);
+    }
+}
